f11e5ce15b144f7dcf4b770f4289b9d255722fbd
kiqueangulo <kiqueangulo@gmail.com>
2022-04-14 02:19:32 -0400

    Array shuffle added, so the words does not load in the same order

--- start.js
+++ start.js
@@ -18,6 +18,8 @@ async function start() {
     };
     
     function loadData(arr) {
+        shuffle(arr);
+
         // Loading the hidden words; 5 words is the easiest level
         for (let i = 0; i < 5; i++) {
             let letters = arr[i].split(''); // Leaving a letter uncover'll be easier by separating each word
@@ -33,6 +35,20 @@ async function start() {
             divOut.appendChild(divIn);
             rightContainer.appendChild(divOut);
         };
+    };
+
+    // Randomize the array with the Fisher-Yates Shuffle, so the words are not loaded in the same order everytime
+    function shuffle(arr) {
+        let current = arr.length,  random;
+      
+        while (current != 0) {
+            random = Math.floor(Math.random() * current);
+            current--;
+      
+            [arr[current], arr[random]] = [arr[random], arr[current]];
+        };
+
+        return arr;
     };
 };
 
